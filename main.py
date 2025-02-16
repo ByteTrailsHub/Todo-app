d48fa4ee1ed1312aa43dbd094a0c730ae79bff3b
Dennis Ebel <bytetrail24@gmail.com>
2025-02-16 19:54:54 +0700

    Update main.py
    Signed-off-by: Dennis Ebel <bytetrail24@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def get_todos():
-    with open('todo.txt', 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+import functions
+import time
 
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print('It is', now)
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -11,15 +12,14 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        with open('todo.txt', 'w') as file:
-            file.writelines(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -30,13 +30,12 @@ while True:
             number = int(user_action[5:])
             number = number-1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            with open('todo.txt', 'w') as file:
-                file.writelines(todos)
+            functions.write_todos(todos)
         except ValueError:
             print('Your command is not valid!')
             continue
@@ -45,13 +44,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop (index)
 
-            with open('todo.txt', 'w') as file:
-                file.writelines(todos)
+            functions.write_todos(todos)
 
             message = f"You've completed {todo_to_remove} and it was removed from the list!"
             print(message)
